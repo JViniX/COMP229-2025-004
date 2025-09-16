e78bc710033b41873fe432142941d47bfec1fd82
JViniX <47263194+JViniX@users.noreply.github.com>
2025-09-16 19:31:22 -0400

    Introduce Components

---
+++ app02/src/IngredientsList.jsx
@@ -0,0 +1,16 @@
+function IngredientsList({ items }) {
+
+    console.log(items);
+    return (
+        <>
+            <h1>Ingredients List</h1>
+            <ul>
+                {items.map((ingredient, i) => (
+                    <li key={i}>{ ingredient }</li>
+                ))}
+            </ul>
+        </>
+    );
+}
+
+export default IngredientsList;
